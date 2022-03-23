3c7f0557086936514dd2f284c02a7cec2616f79e
Kinidario <102186057+Kinidario@users.noreply.github.com>
2022-03-23 11:56:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
+//Bibliotecas
 #include <stdio.h>
 
+//Função
 void Crossbots(int n){
+//Se o número for múltiplo de 3
   if(n%3 == 0){
     printf("Cross");
+//Se o número for múltiplo de 5
     if(n%5 == 0)
       printf("Bots");
   }
+//Se o número for apenas múltiplo de 5
   else if(n%5 == 0)
     printf("Bots");
+//Se ele não for múltiplo de ambos ele printa o próprio número
   else 
     printf("%d", n);
 
 }
 
 int main(void) {
+//Variável
   int n;
   
+//Recebendo o número
   printf("digite um numero : ");
   scanf("%d", &n);
 
+//Chamando a função
   Crossbots(n);
   
   return 0;
